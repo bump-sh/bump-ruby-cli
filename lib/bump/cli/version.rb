fa396d86e5ba84cd0c075034cd10a4128416c27b
Mehdi Lahmam <mehdi@lahmam.com>
2017-11-01 23:34:20 +0100

    Add a CLI skeleton based on Hanami::CLI
    Planned commands are:
    
    - `bump validate`
    - `bump deploy`
    
    Those will probably take some options or have subcommands.

--- lib/bump/cli/version.rb
+++ lib/bump/cli/version.rb
@@ -1,5 +1,5 @@
 module Bump
-  module CLI
+  class CLI
     VERSION = "0.1.0"
   end
 end
--- test/bump/cli_test.rb
+++ test/bump/cli_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
-class Bump::CLITest < Minitest::Test
-  def test_that_it_has_a_version_number
+describe Bump::CLI do
+  before do
+    @original_path = ENV['PATH']
+    ENV['PATH'] = File.expand_path("../../..", __FILE__) + '/exe:' + ENV['PATH']
+  end
+
+  after do
+    ENV['PATH'] = @original_path
+  end
+
+  it "has a version number" do
     refute_nil ::Bump::CLI::VERSION
+  end
+
+  describe "validate command" do
+    it "outputs the right thing" do
+      output = `bump validate`
+
+      assert_equal output.chomp, "Validating..."
+    end
+  end
+
+  describe "deploy command" do
+    it "outputs the right thing" do
+      output = `bump deploy`
+
+      assert_equal output.chomp, "Deploying..."
+    end
   end
 end
