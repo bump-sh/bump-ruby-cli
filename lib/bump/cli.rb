d0b458388133c4ee295cc7540dc4b6643e016351
Mehdi Lahmam <mehdi@lahmam.com>
2017-11-01 23:34:20 +0100

    Add a CLI skeleton based on Hanami::CLI
    Planned commands are:
    
    - `bump validate`
    - `bump deploy`
    
    Those will probably take some options or have subcommands.

--- lib/bump/cli.rb
+++ lib/bump/cli.rb
@@ -1,7 +1,33 @@
 require "bump/cli/version"
+require "hanami/cli"
 
 module Bump
-  module CLI
-    # Your code goes here...
+  class CLI
+    def call(*args)
+      Hanami::CLI.new(Commands).call(*args)
+    end
+
+    module Commands
+      extend Hanami::CLI::Registry
+
+      class Validate < Hanami::CLI::Command
+        desc "Validate API documentation file"
+
+        def call(*)
+          puts "Validating..."
+        end
+      end
+
+      class Deploy < Hanami::CLI::Command
+        desc "Deploy the latest API version docs"
+
+        def call(*)
+          puts "Deploying..."
+        end
+      end
+    end
   end
 end
+
+Bump::CLI::Commands.register "validate", Bump::CLI::Commands::Validate
+Bump::CLI::Commands.register "deploy",   Bump::CLI::Commands::Deploy
