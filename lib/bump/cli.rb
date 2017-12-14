ce2bf52a997b7d5a69a0be0d5cca3298289a6adc
Sébastien Charrier <scharrier@gmail.com>
2017-12-14 10:41:57 +0100

    Remove example commands

--- lib/bump/cli.rb
+++ lib/bump/cli.rb
@@ -9,25 +9,6 @@ module Bump
 
     module Commands
       extend Hanami::CLI::Registry
-
-      class Validate < Hanami::CLI::Command
-        desc "Validate API documentation file"
-
-        def call(*)
-          puts "Validating..."
-        end
-      end
-
-      class Deploy < Hanami::CLI::Command
-        desc "Deploy the latest API version docs"
-
-        def call(*)
-          puts "Deploying..."
-        end
-      end
     end
   end
 end
-
-Bump::CLI::Commands.register "validate", Bump::CLI::Commands::Validate
-Bump::CLI::Commands.register "deploy",   Bump::CLI::Commands::Deploy
