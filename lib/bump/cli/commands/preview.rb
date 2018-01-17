da0db9ef42ee53cf9cb869ada262d7b140a3fd4b
Sébastien Charrier <scharrier@gmail.com>
2018-01-17 10:09:12 +0100

    Extract methods to create a base class

--- lib/bump/cli/commands/preview.rb
+++ lib/bump/cli/commands/preview.rb
@@ -9,18 +9,7 @@ module Bump
         option :format, default: "yaml", values: %w[yaml json], desc: "Format of the definition"
 
         def call(**options)
-          headers = {
-            "Content-Type" => "application/json"
-          }
-
-          body = {
-            preview: {
-              definition: open(options.fetch(:file)).read,
-              format: options.fetch(:format)
-            }
-          }
-
-          response = HTTP.headers(headers).post(API_URL + "/previews", body: body.to_json)
+          response = HTTP.headers(headers(options)).post(API_URL + "/previews", body: body(options).to_json)
           body = JSON.parse(response.body)
 
           if response.code == 201
@@ -36,16 +25,35 @@ module Bump
 
         private
 
+        def headers(options)
+          default_headers
+        end
+
+        def default_headers
+          {
+            "Content-Type" => "application/json"
+          }
+        end
+
+        def body(options)
+          {
+            preview: {
+              definition: open(options.fetch(:file)).read,
+              format: options.fetch(:format)
+            }
+          }
+        end
+
         def display_invalid_definition(body)
-          puts 'Definition is not valid:'
-          body['errors']['raw_definition'].each do |error|
+          puts "Definition is not valid:"
+          body["errors"]["raw_definition"].each do |error|
             puts "> #{error}"
           end
           abort
         end
 
         def display_generic_error(body)
-          abort body['message']
+          abort body["message"]
         end
 
         def display_http_error(error)
