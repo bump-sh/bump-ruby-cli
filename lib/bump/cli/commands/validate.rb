0bb8911c906a93381f5efcaa003f780d8f8e0518
Sébastien Charrier <scharrier@gmail.com>
2020-03-27 14:59:34 +0100

    Add hubs support
    Support the hub parameter, and hubs tokens.

--- lib/bump/cli/commands/validate.rb
+++ lib/bump/cli/commands/validate.rb
@@ -7,7 +7,8 @@ module Bump
         desc "Validate a given file against its schema definition"
         argument :file, required: true, desc: "Path or URL to your API documentation file. OpenAPI (2.0 to 3.0.2) and AsyncAPI (2.0) specifications are currently supported."
         option :documentation, default: ENV.fetch("BUMP_ID", ""), desc: "Documentation public id or slug"
-        option :token, default: ENV.fetch("BUMP_TOKEN", ""), desc: "Documentation private token"
+        option :hub, desc: "Hub id or slug"
+        option :token, default: ENV.fetch("BUMP_TOKEN", ""), desc: "Documentation or Hub token"
         option :specification, desc: "Specification of the definition"
         option :validation, desc: "Validation mode", values: %w(basic strict), default: 'basic'
 
