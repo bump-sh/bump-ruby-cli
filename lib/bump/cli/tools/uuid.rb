4d9814e760787263e4d5e4c464a217ad654239a2
Sébastien Charrier <scharrier@gmail.com>
2020-03-27 17:52:50 +0100

    Keep Ruby 2.3 compatibility
    Replace .match? with =~ and replace the Hash#compact.

--- lib/bump/cli/tools/uuid.rb
+++ lib/bump/cli/tools/uuid.rb
@@ -5,7 +5,7 @@ module Bump
         REGEXP = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
 
         def self.valid?(string)
-          string.to_s.match?(REGEXP)
+          string.to_s =~ REGEXP
         end
       end
     end
