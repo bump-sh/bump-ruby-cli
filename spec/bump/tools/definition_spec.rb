3ba1c757289cbd21ce19d58397106a9e823a224e
Sébastien Charrier <scharrier@gmail.com>
2020-06-02 18:07:26 +0200

    Parse and load YAML or JSON definition

--- spec/bump/tools/definition_spec.rb
+++ spec/bump/tools/definition_spec.rb
@@ -11,5 +11,27 @@ describe Bump::CLI::Tools::Definition do
       expect(definition).to have_received('open').once.with('path/to/file')
       expect(file).to have_received('read').once
     end
+
+    it 'loads the definition and import references' do
+      definition = Bump::CLI::Tools::Definition.new('path/to/file', import_external_references: true)
+      allow(definition).to receive(:parse_file_and_import_external_references).and_return('content')
+
+      expect(definition.prepare).to eq('content')
+      expect(definition).to have_received(:parse_file_and_import_external_references).once
+    end
+
+    it 'loads YAML definition and serializes it correctly' do
+      definition = Bump::CLI::Tools::Definition.new('path/to/file', import_external_references: true)
+      allow(definition).to receive(:read_file).and_return('property: value')
+
+      expect(definition.prepare).to include('property: value')
+    end
+
+    it 'loads JSON definition and serializes it correctly' do
+      definition = Bump::CLI::Tools::Definition.new('path/to/file', import_external_references: true)
+      allow(definition).to receive(:read_file).and_return('{"property": "value"}')
+
+      expect(definition.prepare).to include('{"property":"value"}')
+    end
   end
 end
