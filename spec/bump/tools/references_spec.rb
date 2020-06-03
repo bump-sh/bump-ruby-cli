8b2ccf0d19f4bf7418c2c2937a536d30c856c4db
Sébastien Charrier <scharrier@gmail.com>
2020-06-03 13:59:40 +0200

    Support subpaths in external

--- spec/bump/tools/references_spec.rb
+++ spec/bump/tools/references_spec.rb
@@ -63,6 +63,21 @@ describe Bump::CLI::Tools::References do
       expect(references).to have_received(:open).with('/somewhere/some/internal/path.yml')
     end
 
+    it 'supports paths with subpaths' do
+      references = Bump::CLI::Tools::References.new({
+        'hello' => {
+          '$ref' => 'some/internal/path.yml#subpath/subsub'
+        }
+      }, base_path: '/somewhere/base.yml')
+      allow(references).to receive(:open).and_return(spy(read: 'content'))
+
+      references.import!
+
+      expect(references.definition.dig('hello', '$ref')).to eq('#/components/x-imported/1/subpath/subsub')
+      expect(references.definition.dig('components', 'x-imported', '1')).to eq('content')
+      expect(references).to have_received(:open).with('/somewhere/some/internal/path.yml')
+    end
+
     it 'supports absolute paths' do
       references = Bump::CLI::Tools::References.new({
         'hello' => {
@@ -86,6 +101,21 @@ describe Bump::CLI::Tools::References do
 
       references.import!
 
+      expect(references).to have_received(:open).with('http://example.com/file.xml')
+    end
+
+    it 'supports URI with subpaths' do
+      references = Bump::CLI::Tools::References.new({
+        'hello' => {
+          '$ref' => 'http://example.com/file.xml#subpath/subsub'
+        }
+      }, base_path: '/somewhere/base.yml')
+      allow(references).to receive(:open).and_return(spy(read: 'content'))
+
+      references.import!
+
+      expect(references.definition.dig('hello', '$ref')).to eq('#/components/x-imported/1/subpath/subsub')
+      expect(references.definition.dig('components', 'x-imported', '1')).to eq('content')
       expect(references).to have_received(:open).with('http://example.com/file.xml')
     end
   end
